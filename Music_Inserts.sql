390c086e24fc7fd9aec32eb58f3dae64adf6078b
Michael Ignatev <ignatevmc94@gmail.com>
2025-05-19 21:48:57 +0300

    Update Music_Inserts.sql

--- Music_Inserts.sql
+++ Music_Inserts.sql
@@ -12,7 +12,8 @@ INSERT INTO Artist VALUES
 (2, 'Michael Jackson'),
 (3, 'Eminem'),
 (4, 'Daft Punk'),
-(5, 'Queen');
+(5, 'Queen'),
+(6, 'Taylor Swift');
 
 -- Вставка альбомов
 INSERT INTO Album VALUES
@@ -20,7 +21,8 @@ INSERT INTO Album VALUES
 (2, 'Thriller', 1982),
 (3, 'The Marshall Mathers LP', 2000),
 (4, 'Random Access Memories', 2013),
-(5, 'A Night at the Opera', 1975);
+(5, 'A Night at the Opera', 1975),
+(6, 'Lover', 2019);
 
 -- Вставка треков
 INSERT INTO Track VALUES
@@ -33,7 +35,8 @@ INSERT INTO Track VALUES
 (7, 'Get Lucky', 369, 4),
 (8, 'Instant Crush', 337, 4),
 (9, 'Bohemian Rhapsody', 354, 5),
-(10, 'You''re My Best Friend', 172, 5);
+(10, 'You''re My Best Friend', 172, 5),
+(11, 'Anti-Hero', 201, 6);
 
 -- Связи исполнителей с альбомами
 INSERT INTO Album_Artist VALUES
@@ -42,7 +45,8 @@ INSERT INTO Album_Artist VALUES
 (3, 3, 3),   -- Eminem - The Marshall Mathers LP
 (4, 4, 4),   -- Daft Punk - Random Access Memories
 (5, 5, 5),   -- Queen - A Night at the Opera
-(6, 1, 5);   -- Queen также участвовали в Abbey Road
+(6, 1, 5),   -- Queen + в Abbey Road
+(7, 6, 6);   -- Taylor Swift - Lover
 
 -- Связи исполнителей с жанрами
 INSERT INTO Artist_Genre VALUES
@@ -52,7 +56,8 @@ INSERT INTO Artist_Genre VALUES
 (4, 3, 3),   -- Eminem - Хип-хоп
 (5, 4, 4),   -- Daft Punk - Электроника
 (6, 5, 1),   -- Queen - Рок
-(7, 5, 5);   -- Queen - также Джаз
+(7, 5, 5),   -- Queen - также Джаз
+(8, 6, 2);	 -- Taylor Swift - Поп
 
 -- Вставка сборников
 INSERT INTO Collection VALUES
@@ -72,4 +77,6 @@ INSERT INTO Collection_Track VALUES
 (7, 3, 7),   -- Get Lucky в В спортзал
 (8, 4, 8),   -- Instant Crush в Для души
 (9, 4, 2),   -- Something в Для души
-(10, 4, 9);  -- Bohemian Rhapsody в Для души
+(10, 4, 9),  -- Bohemian Rhapsody в Для души
+(11, 4, 11), -- Anti-Hero в Для души
+(12, 3, 11); -- Anti-Hero В спортзал
